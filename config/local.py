01750f667885dd60b2afd77ca0ac97f30ca6a5f0
Sergio Perez <sperez4mba@gmail.com>
2017-12-13 11:08:53 +0100

    Added error msg and fixes

--- config/local.py
+++ config/local.py
@@ -7,14 +7,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__name__))
 DEBUG = False
 TESTING = False
 
-SQLALCHEMY_DATABASE_URI = "{}{}{}{}".format(
+SQLALCHEMY_DATABASE_URI = "{}{}/{}".format(
     'sqlite:///',
     BASE_DIR,
-    '/',
     'url_shortener.sqlite3'
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SERVER_NAME = "{}:{}".format(
-    os.environ.get('SERVER_HOST'),
-    os.environ.get('SERVER_PORT')
-)
--- config/test.py
+++ config/test.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-
-
 DEBUG = False
 TESTING = True
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = (
-    'sqlite://'
-)
+SQLALCHEMY_DATABASE_URI = "sqlite://"
